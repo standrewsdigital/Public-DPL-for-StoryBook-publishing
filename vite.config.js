b30b3e6c2a8704af4b3435f928e6fae635f2eab3
Tommy Oldfield <tco1@st-andrews.ac.uk>
2026-05-07 10:12:22 +0100

    FEAT Vite directory list plugin for easy navigation

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { directoryPlugin } from 'vite-plugin-list-directory-contents'
 
 export default defineConfig({
   build: {
     outDir: 'dist-vite',
     rollupOptions: {
-      input: 'scripts/form-elements.js',
+      input: 'scripts/form-elements.js'
     },
     sourcemap: true
-  }
-});
+  },
+  plugins: [directoryPlugin({
+    baseDir: __dirname,
+    filterList: ['.md', '.js', 'scss-styles', 'node_modules', '.git', '.vscode', 'compiled-css', 'assets', 'package.json', 'package-lock.json', 'dist-vite', 'scripts', '.gitignore', '.gitlab-ci.yml', 'NPMVERSION.md', 'README.md', 'index.js', 'uploadToFtps.cjs', 'vite.config.js', '.env']
+  })]
+})
