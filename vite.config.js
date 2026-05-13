0f529e6a0ef69d421389761854678726aa95e75a
sta-digital-communications <digitalcommunications@st-andrews.ac.uk>
2026-05-13 08:07:01 +0100

    Updated vite config to only point to html-examples

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from 'node:path'
 import { directoryPlugin } from 'vite-plugin-list-directory-contents'
 
 export default defineConfig({
@@ -9,8 +10,16 @@ export default defineConfig({
     },
     sourcemap: true
   },
-  plugins: [directoryPlugin({
-    baseDir: __dirname,
-    filterList: ['.md', '.js', 'scss-styles', 'node_modules', '.git', '.vscode', 'compiled-css', 'assets', 'package.json', 'package-lock.json', 'dist-vite', 'scripts', '.gitignore', '.gitlab-ci.yml', 'NPMVERSION.md', 'README.md', 'index.js', 'uploadToFtps.cjs', 'vite.config.js', '.env']
-  })]
+
+  plugins: [
+    directoryPlugin({
+      baseDir: path.resolve(__dirname, 'html-examples'),
+
+      filterList: [
+        '.git',
+        '.vscode',
+        'node_modules'
+      ]
+    })
+  ]
 })
