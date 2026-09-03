920d95b0783c4ab40057f00d433666b3fd887dc0
Tommy Oldfield <tco1@st-andrews.ac.uk>
2026-09-03 10:57:11 +0100

    FEAT button - additional story params

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,16 +1,53 @@
 export const createButton = ({
-  colour,
+  colour = 'blue',
   label,
+  type = 'Primary',
+  size = 'Regular',
+  cornerShape = 'Square',
 }) => {
-  colour = colour === 'blue' ? '' : colour;
 
   const button = document.createElement('button');
+
+  switch (type) {
+    case 'Action':
+      button.classList.add('action');
+      break;
+    case 'Primary':
+    default:
+      break;
+  }
+
+  switch (size) {
+    case 'Large':
+      button.classList.add('lg');
+      break;
+    case 'Regular':
+    default:
+      break;
+  }
+
+  switch (cornerShape) {
+    case "Round - Small":
+      button.classList.add('radius-sm');
+      break;
+    case "Round - Medium":
+      button.classList.add('radius-md');
+      break;
+    case "Round - Large":
+      button.classList.add('radius-lg');
+      break;
+    case "Round - Extra large":
+      button.classList.add('radius-xl');
+      break;
+    case 'Square':
+    default:
+      break;
+  }
+
   button.type = 'button';
   button.innerText = label;
   button.classList.add('btn');
-  if (colour) {
-    button.classList.add(colour);
-  }
+  button.classList.add(colour.toLowerCase());
 
   return button;
 };
--- src/components/button/Button.stories.js
+++ src/components/button/Button.stories.js
@@ -3,21 +3,36 @@ import { createButton } from './Button';
 export default {
   title: 'Components/Button',
   tags: ['autodocs'],
-  render: ({ colour, label }) => {
-    return createButton({ colour, label });
+  render: ({ colour, label, size, type, cornerShape }) => {
+    return createButton({ colour, label, size, type, cornerShape });
   },
   argTypes: {
+    type: {
+      control: { type: 'select' },
+      options: ['Primary', 'Action', 'Dropdown']
+    },
+    size: {
+      control: { type: 'select' },
+      options: ['Regular', 'Large']
+    },
     colour: {
       control: { type: 'select' },
-      options: ['blue', 'red', 'orange', 'green', 'burgundy', 'grey', 'purple', 'white']
+      options: ['Blue', 'Red', 'Orange', 'Green', 'Burgundy', 'Grey', 'Purple', 'White']
     },
-    label: { control: 'text' }
+    label: { control: 'text' },
+    cornerShape: {
+      control: { type: 'select' },
+      options: ['Square', 'Round - Small', 'Round - Medium', 'Round - Large', 'Round - Extra large']
+    },
   }
 }
 
 export const Primary = {
   args: {
-    colour: 'blue',
-    label: 'Click me!'
+    type: 'primary',
+    size: 'Large',
+    colour: 'Blue',
+    label: 'Click me!',
+    cornerShape: 'Square'
   }
 }
