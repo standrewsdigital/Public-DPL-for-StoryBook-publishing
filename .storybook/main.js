da30e8a4169bda01eb8fc996e8c3d2b7e4df017a
sta-digital-communications <digitalcommunications@st-andrews.ac.uk>
2026-09-07 15:00:38 +0100

    Adding @storybook/addon-designs dependency and design examples for alert.

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,7 +10,8 @@ const config = {
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
+    "@storybook/addon-designs"
   ],
   "framework": "@storybook/html-vite",
   "staticDirs": ['../assets'],
--- src/components/alert/Alert.stories.js
+++ src/components/alert/Alert.stories.js
@@ -35,7 +35,13 @@ export const Success = {
     headingLevel: 'h2',
     headingContent: 'Thank you!',
     content: `<p>Thank you for submitting your details. If you have any questions please email <a href="mailto:#">example@st-andrews.ac.uk</a>.</p>`
-  }
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/hnuhF4nQEY4oK7HicfBzLD/Untitled?node-id=1-123&t=ubP9TOof6owVlVj0-4',
+    },
+  },
 }
 
 export const Info = {
@@ -44,7 +50,13 @@ export const Info = {
     headingLevel: 'h2',
     headingContent: 'Did you know?',
     content: `<p>University of St Andrews was founded over 600 years ago. Find out more about the <a href='#'>history of St Andrews</a>.</p>`
-  }
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/hnuhF4nQEY4oK7HicfBzLD/Untitled?node-id=1-123&t=ubP9TOof6owVlVj0-4',
+    },
+  },
 }
 
 export const Warning = {
@@ -53,7 +65,13 @@ export const Warning = {
     headingLevel: 'h2',
     headingContent: 'Warning!',
     content: `<p>The deadline for submissions is the end of Semester 2. If you need help, please email <a href='mailto:#'>example@st-andrews.ac.uk</a>.</p>`
-  }
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/hnuhF4nQEY4oK7HicfBzLD/Untitled?node-id=1-123&t=ubP9TOof6owVlVj0-4',
+    },
+  },
 }
 
 export const Danger = {
@@ -62,5 +80,30 @@ export const Danger = {
     headingLevel: 'h2',
     headingContent: 'Submissions have ended',
     content: `<p>The deadline for submissions has passed. If you need help please email <a href='mailto:#'>example@st-andrews.ac.uk</a>.</p>`
-  }
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/hnuhF4nQEY4oK7HicfBzLD/Untitled?node-id=1-123&t=ubP9TOof6owVlVj0-4',
+    },
+  },
 }
+
+export const FigmaEmbedDemo = {
+  name: 'Figma embed demo',
+
+  args: {
+    option: 'info',
+    headingLevel: 'h2',
+    headingContent: 'A short title is best',
+    content:
+      '<p>A description should be a short, complete sentence.</p>',
+  },
+
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/hnuhF4nQEY4oK7HicfBzLD/Untitled?node-id=1-123&t=ubP9TOof6owVlVj0-4',
+    },
+  },
+};
