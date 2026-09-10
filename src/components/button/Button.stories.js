cbe1c78b2c0f2a15bfdebdf5feec5a30ab13e05b
Tommy Oldfield <tco1@st-andrews.ac.uk>
2026-09-10 11:53:13 +0100

    DOCS button documentation, added all btn colour variants

--- src/components/button/Button.stories.js
+++ src/components/button/Button.stories.js
@@ -1,9 +1,23 @@
 import { createButton } from './Button';
 import '../../../scripts/dropdown-button.js';
 
+const colours = ['Blue', 'Red', 'Orange', 'Green', 'Burgundy', 'Grey', 'Purple', 'White'];
+
+function renderAllColours(args) {
+  const wrapper = document.createElement('div');
+  wrapper.style.display = 'flex';
+  wrapper.style.flexWrap = 'wrap';
+  wrapper.style.gap = '1rem';
+
+  colours.forEach((colour) => {
+    wrapper.appendChild(createButton({ ...args, colour, label: colour }));
+  })
+  return wrapper;
+}
+
 export default {
   title: 'Components/Button',
-  tags: ['autodocs'],
+  tags: ['!autodocs'],
   render: ({ colour, label, size, type, cornerShape, linkList }) => {
     return createButton({ colour, label, size, type, cornerShape, linkList });
   },
@@ -18,7 +32,7 @@ export default {
     },
     colour: {
       control: { type: 'select' },
-      options: ['Blue', 'Red', 'Orange', 'Green', 'Burgundy', 'Grey', 'Purple', 'White']
+      options: colours
     },
     label: { control: 'text' },
     cornerShape: {
@@ -51,18 +65,88 @@ export default {
 export const Primary = {
   args: {
     type: 'Primary',
-    size: 'Large',
+    size: 'Regular',
     colour: 'Blue',
     label: 'Click me!',
     cornerShape: 'Square'
   }
 }
 
+export const PrimaryLarge = {
+  args: {
+    ...Primary.args,
+    size: 'Large',
+  }
+}
+
+export const PrimaryColours = {
+  render: renderAllColours,
+  args: {
+    ...Primary.args
+  },
+  tags: ['!dev']
+}
+
+export const Action = {
+  args: {
+    ...Primary.args,
+    type: 'Action',
+  }
+}
+
+export const ActionLarge = {
+  args: {
+    ...Action.args,
+    size: 'Large',
+  }
+}
+
+export const ActionSecondary = {
+  args: {
+    ...ActionLarge.args,
+    colour: 'White',
+  }
+}
+
+export const ActionColours = {
+  render: renderAllColours,
+  args: {
+    ...Action.args
+  },
+  tags: ['!dev']
+}
+
 export const Dropdown = {
   args: {
+    ...Primary.args,
     type: 'Dropdown',
+  },
+  parameters: {
+    docs: {
+      story: {
+        inline: false,
+        iframeHeight: 200,
+      },
+    },
+  },
+}
+export const DropdownLarge = {
+  args: {
+    ...Dropdown.args,
     size: 'Large',
-    cornerShape: 'Square',
-    label: 'Click me!'
+  },
+  parameters: {
+    ...Dropdown.parameters
+  }
+}
+
+export const DropdownColours = {
+  render: renderAllColours,
+  args: {
+    ...Dropdown.args
+  },
+  tags: ['!dev'],
+  parameters: {
+    ...Dropdown.parameters
   }
 }
