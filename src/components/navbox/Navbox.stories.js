406d1a8d3d5381c67ef909a0510bcb30451f736d
Tommy Oldfield <tco1@st-andrews.ac.uk>
2026-09-11 12:54:30 +0100

    Navbox - Add stories for 1, 2, and 3 navboxes in grids

--- src/components/navbox/Navbox.stories.js
+++ src/components/navbox/Navbox.stories.js
@@ -31,32 +31,152 @@ export default {
 
 export const Navbox = {
   args: {
-    image: './750x500.png',
-    altText: '750x500 placeholder',
-    url: 'https://www.st-andrews.ac.uk/',
-    title: 'Navbox title',
-    content: '<p>Lorem ipsum</p>',
-    columns: 1,
+    contentArray: [
+      {
+        url: 'https://www.st-andrews.ac.uk/',
+        title: 'Navbox title',
+      }
+    ],
+    size: 'Regular',
+  }
+}
+
+export const NavboxTextOnly = {
+  name: 'Navbox - Text only',
+  args: {
+    contentArray: [
+      {
+        url: 'https://www.st-andrews.ac.uk/',
+        title: 'Navbox title',
+        content: '<p>Lorem ipsum</p>',
+      }
+    ],
+    size: 'Regular',
+  }
+}
+
+export const NavboxImageOnly = {
+  name: 'Navbox - Image only',
+  args: {
+    contentArray: [
+      {
+        url: 'https://www.st-andrews.ac.uk/',
+        title: 'Navbox title',
+        image: './750x500.png',
+        altText: '750x500 placeholder',
+      }
+    ],
+    size: 'Regular',
+  }
+}
+
+export const NavboxTextAndImage = {
+  name: 'Navbox - Text and Image',
+  args: {
+    contentArray: [
+      {
+        url: 'https://www.st-andrews.ac.uk/',
+        title: 'Navbox title',
+        content: '<p>Lorem ipsum</p>',
+        image: './750x500.png',
+        altText: '750x500 placeholder',
+      }
+    ],
+    size: 'Regular',
   }
 }
 
 export const GridTwo = {
+  name: 'Grid - Two navboxes',
   args: {
-    image: './750x500.png',
-    altText: '750x500 placeholder',
-    url: 'https://www.st-andrews.ac.uk/',
-    title: 'Navbox title',
-    content: '<p>Lorem ipsum</p>',
-    columns: 2,
+    contentArray: [
+      ...Navbox.args.contentArray,
+      ...Navbox.args.contentArray
+    ],
+    size: 'Large',
   }
 }
-export const GridThree = {
+
+export const GridTwoTextOnly = {
+  name: 'Grid - Two navboxes, text only',
   args: {
-    image: './750x500.png',
-    altText: '750x500 placeholder',
-    url: 'https://www.st-andrews.ac.uk/',
-    title: 'Navbox title',
-    content: '<p>Lorem ipsum</p>',
-    columns: 3,
+    contentArray: [
+      ...NavboxTextOnly.args.contentArray,
+      ...NavboxTextOnly.args.contentArray
+    ],
+    size: 'Large',
+  }
+}
+
+export const GridTwoImageOnly = {
+  name: 'Grid - Two navboxes, image only',
+  args: {
+    contentArray: [
+      ...NavboxImageOnly.args.contentArray,
+      ...NavboxImageOnly.args.contentArray
+    ],
+    size: 'Large',
+  }
+}
+
+
+
+export const GridTwoTextAndImage = {
+  name: 'Grid - Two navboxes, text and Image',
+  args: {
+    contentArray: [
+      ...NavboxTextAndImage.args.contentArray,
+      ...NavboxTextAndImage.args.contentArray,
+    ],
+    size: 'Large',
+  }
+}
+
+export const GridThree = {
+  name: 'Grid - Three navboxes',
+  args: {
+    contentArray: [
+      ...Navbox.args.contentArray,
+      ...Navbox.args.contentArray,
+      ...Navbox.args.contentArray
+    ],
+    size: 'Regular'
+  }
+}
+
+export const GridThreeTextOnly = {
+  name: 'Grid - Three navboxes, text only',
+  args: {
+    contentArray: [
+      ...NavboxTextOnly.args.contentArray,
+      ...NavboxTextOnly.args.contentArray,
+      ...NavboxTextOnly.args.contentArray,
+    ],
+    size: 'Regular',
+  }
+}
+
+
+export const GridThreeImageOnly = {
+  name: 'Grid - Three navboxes, image only',
+  args: {
+    contentArray: [
+      ...NavboxImageOnly.args.contentArray,
+      ...NavboxImageOnly.args.contentArray,
+      ...NavboxImageOnly.args.contentArray,
+    ],
+    size: 'Regular',
+  }
+}
+
+export const GridThreeTextAndImage = {
+  name: 'Grid - Three navboxes, text and Image',
+  args: {
+    contentArray: [
+      ...NavboxTextAndImage.args.contentArray,
+      ...NavboxTextAndImage.args.contentArray,
+      ...NavboxTextAndImage.args.contentArray,
+    ],
+    size: 'Regular',
   }
 }
